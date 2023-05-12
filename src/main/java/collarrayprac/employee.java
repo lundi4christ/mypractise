1f63917b0f895bffba06d796c07c339a2c4d9189
lundi4christ <lundi4christ@gmail.com>
2023-05-12 09:34:32 +0100

    ANOTHercommit

---
+++ src/main/java/collarrayprac/employee.java
@@ -0,0 +1,13 @@
+package collarrayprac;
+
+import lombok.Data;
+
+@Data
+public class employee {
+
+    private String firstname;
+    private String lastname;
+    private int age;
+    private String email;
+
+}
---
+++ src/main/java/collarrayprac/looping.java
@@ -0,0 +1,40 @@
+package collarrayprac;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class looping {
+
+    public static void main (String[] args){
+
+        employee emp1 = new employee();
+        emp1.setFirstname("mark");
+        emp1.setLastname("james");
+        emp1.setAge(20);
+        emp1.setEmail("mark@gmail.com");
+
+        employee emp2 = new employee();
+        emp2.setFirstname("luke");
+        emp2.setLastname("andrew");
+        emp2.setAge(30);
+        emp2.setEmail("luke@gmail.com");
+
+        List<employee> empout = new ArrayList<>();
+
+        empout.add(emp1);
+        empout.add(emp2);
+
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            String str = mapper.writeValueAsString(empout);
+            System.out.println(str);
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+    }
+}
