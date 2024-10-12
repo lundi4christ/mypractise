3ee8ca82fc9e01304d8a8b1e2ff8f01bf0c8f761
lundi4christ <monday.stephen@unionsystems.com>
2024-10-12 22:01:34 +0100

    newdatecommit2

--- src/main/java/dataclasses/TestDate.java
+++ src/main/java/dataclasses/TestDate.java
@@ -1,13 +1,17 @@
 package dataclasses;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 public class TestDate {
 
     public static void main (String[] args) {
 
         LocalDate getdate = LocalDate.now();
+        System.out.println("***for LocalDAte *** - " + getdate);
 
-        System.out.println(getdate);
+        Date date = new Date();
+        System.out.println("***for date *** - " + date);
+
     }
 }
