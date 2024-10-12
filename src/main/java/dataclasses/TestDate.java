6d7a6369bcc715db33245c7031e64f41b1ca65b3
lundi4christ <monday.stephen@unionsystems.com>
2024-10-12 21:57:58 +0100

    newdatecommit

---
+++ src/main/java/dataclasses/TestDate.java
@@ -0,0 +1,13 @@
+package dataclasses;
+
+import java.time.LocalDate;
+
+public class TestDate {
+
+    public static void main (String[] args) {
+
+        LocalDate getdate = LocalDate.now();
+
+        System.out.println(getdate);
+    }
+}
